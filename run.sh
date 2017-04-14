ad9e24e3d0fec784819d6a947e217d03db3baefd
rhpelorosso <rodrigo.pelorosso@gmail.com>
2017-04-14 12:09:07 +0000

    Various fixes to the first commit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/opt/paraview/bin/pvpython -dr "/home/silencer/Desktop/nodejs/light-viz/server/pvw-light-viz.py" --content "/home/silencer/Desktop/nodejs/hornero/dist/" --port 8080
+/opt/paraview/bin/pvpython -dr "/home/silencer/Desktop/nodejs/light-viz/server/pvw-light-viz.py" --content "/home/silencer/Desktop/nodejs/blankParaviewProject/dist/" --port 8080
 
